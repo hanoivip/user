62797e19963625ebbb637ff1e59ab62f5487502b
unknown <game.oh.vn@gmail.com>
2023-12-18 09:24:05 +0700

    f

--- views/user.blade.php
+++ views/user.blade.php
@@ -1,7 +1,16 @@
 @extends('hanoivip::layouts.app-id')
 
 @section('content')
-
-
+<div class="zid_pagecont">     
+    <h2>Trạng thái an toàn
+    	@if ($status)
+    		<img src="{{asset('/id/images/success.png')}}" width="32"/>
+    	@else
+    		<img src="{{asset('/id/images/failure.png')}}" width="32"/>
+    	@endif
+    </h2>
+	
+    @include('hanoivip::user-menu')
+</div>
 
 @endsection
